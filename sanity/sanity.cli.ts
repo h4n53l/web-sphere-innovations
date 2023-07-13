046a998857adefa3f4856e222adbee9dbeaa006c
= Jonathan Hansel Awo <= jonhan17@gmail.com>
2023-07-13 22:55:15 +0100

    Moved sanity

--- sanity/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'nfvjxp0o',
-    dataset: 'production'
-  }
-})
--- sanity/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-import projects from "./projects";
-import reviews from "./reviews";
-import services from "./services";
-
-export const schemaTypes = [services, projects, reviews]
